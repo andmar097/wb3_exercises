1f63b274c30c1485d00e62a47fb4fd3ffc17f1cf
Andrea Martinez <mandrea781@outlook.com>
2024-04-30 13:04:36 -0500

    completed simple_functions.js

---
+++ functions/simple_functions.js
@@ -0,0 +1,29 @@
+"use strict"
+
+//function to display my name, favorite movie, and favorite band
+function favoriteThings (){
+
+console.log("My name is Andrea");
+console.log("My favorite movie is Dune 2");
+console.log("My favorite band is BTS");
+
+}
+
+function whyImHere () {
+
+console.log("I am here because I was testing out the waters to a new field");
+
+}
+
+function favoritePlace () {
+
+console.log("My favorite place is 10560 Walnut St #200, Dallas, TX 75243")
+
+}
+
+
+
+//calling/invoking our function
+favoriteThings ();
+whyImHere();
+favoritePlace();
